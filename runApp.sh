53a437b4ea1f3bb28e710fcbf5b007879ef811a5
Johnson Abraham <abraham@uk.ibm.com>
2021-02-13 11:00:08 +0000

    updating the run script and updating readme.md instruction

--- runApp.sh
+++ runApp.sh
@@ -2,13 +2,17 @@
 
 print_help() {
 	echo "--start | -s : start the applications"
-	echo "--stop | -x: stop the applications"
+	echo "--stop | -x : stop the applications"
+	echo "--remove | -r : removes the images forcefully"
 }
 
 if [[ $1 == '--start' || $1 == '-s' ]]; then
 	docker-compose --file=docker-compose-remote.yml up -d --remove-orphans
 elif [[ $1 == '--stop' || $1 == '-x' ]]; then
-	docker-compose down --remove-orphans
+	docker-compose --file=docker-compose-remote.yml down --remove-orphans
+elif [[ $1 == '--remove' || $1 == '-r' ]]; then
+	docker rmi --force johnson2ajar/vending_machine:vending_ui
+	docker rmi --force johnson2ajar/vending_machine:vending_service
 elif [[ $1 == '--help' || $1 == '-h' ]]; then
 	print_help
 else
